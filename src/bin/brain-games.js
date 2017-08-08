e44c086ed1030cd1a0f815a22a99041d0b2ce105
GrammPozitiva <tanuxxxa_s@mail.ru>
2017-08-08 08:01:55 +0300

    getName added

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
+import getName from '../';
 
 console.log('Welcome to the Brain Games!');
+getName();
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+
+const getName = () =>{
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+}
+
+export default getName;
