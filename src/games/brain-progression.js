7e4ea47e8afd0db72dffc1aadf8b660c3232b1ef
GrammPozitiva <tanuxxxa_s@mail.ru>
2017-08-12 11:13:44 +0300

    refactor code brain-progress.js

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -2,27 +2,28 @@ import engine from '..';
 import generateNumber from '../utils';
 
 const rules = 'What number is missing in this progression?';
-const formArray = (diapozoneStart,progression) => {
+const formArray = (diapozoneStart, progression) => {
   const arr = [];
-  for(let i = 0; i < 10;i++){
-    arr.push(diapozoneStart);
-    diapozoneStart+=progression;
+  let number = diapozoneStart;
+  for (let i = 0; i < 10; i += 1) {
+    arr.push(number);
+    number += progression;
   }
   return arr;
 };
-const getCorrectAnswer = (arr) => {
-  return arr[generateNumber(2,8)];
-};
+const getCorrectAnswer = arr => arr[generateNumber(2, 8)];
+
 const getQuestion = (arr, x) => {
-  return arr.map((el,index) => {
-     if (el === x) {
-        el = '..';
-     }
-     return el;
-   }).join(' ');
+  const question = arr.map((el) => {
+    if (el === x) {
+      return '..';
+    }
+    return el;
+  }).join(' ');
+  return question;
 };
 const getStepData = () => {
-  const formaleteArr = formArray(generateNumber(1, 50), generateNumber(1,10))
+  const formaleteArr = formArray(generateNumber(1, 50), generateNumber(1, 10));
   const correct = getCorrectAnswer(formaleteArr);
   const inputQuestion = getQuestion(formaleteArr, correct);
   return {
